4a6af895ee625bd4d100962de77bf913de322d43
Vincent Abolarin <vincentabolarin@gmail.com>
2022-03-02 07:32:20 +0100

    initialise project

---
+++ vendly/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="main">
+        Hello World!
+      </div>
+    </div>
+  );
+}
+
+export default App;
